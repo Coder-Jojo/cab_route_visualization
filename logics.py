b82294f2a59dde29cff9a470a3d47819c53118b3
ayushit5914 <71024846+ayushit5914@users.noreply.github.com>
2022-04-18 02:49:18 +0530

    Update logics.py

--- logics.py
+++ logics.py
@@ -4,3 +4,12 @@ from algorithms.grid_logic import create_road
 
 def logic(grid):
     create_road(grid, 40, 20)
+    start_x=int(input())
+    start_y=int(input())
+    end_x=int(input())
+    end_y=int(input())
+    #x=adjacency.g_value(grid)
+    start=adjacency.Node(grid, loc=(start_y,start_x))
+    end=adjacency.Node(grid, loc=(end_y, end_x))
+    closedList = adjacency.search_path(start, end, grid)
+    print(adjacency.path(start, closedList, grid, end))
